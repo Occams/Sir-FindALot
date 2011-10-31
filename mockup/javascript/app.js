bb0de7c42df1ade3dae3d807659ef4e02661f2b8
Huber Bastian <huber@mint>
2011-10-31 16:41:15 +0100

    Refactor

--- mockup/javascript/app.js
+++ mockup/javascript/app.js
@@ -15,38 +15,73 @@ var Color = {
   }
 };
 
-var Geolocate = {
-  init: function() {
-    $("#foo").bind("pageshow", function() {
-      Geolocate.setLoading(true);
-      setTimeout(Geolocate.success, 3000);
-    }).trigger("pageshow");
+var ProgressBar = {
+  speed: 600,
+  easing: "linear",
+
+  init: function(el) {
+    el = $(el).empty().append("<div class=\"ui-progressbar-value ui-btn-corner-all\"></div>");
+    el.addClass("ui-progressbar ui-btn-corner-all").show();
     
-    $("#geolocate").bind('click', function() {$("#foo").trigger("pageshow");});
+    slider = el.find(".ui-progressbar-value");
+    slider.moveLeft = function() { slider.animate({left:"0%"}, ProgressBar.speed, ProgressBar.easing, slider.moveRight); };
+    slider.moveRight = function() { slider.animate({left:"80%"}, ProgressBar.speed, ProgressBar.easing, slider.moveLeft); };
+    slider.moveLeft();
   },
-  setLoading: function(enable) {
-    Geolocate.loadingStatus = 1;
-    if(enable) {
-      Geolocate.interval = setInterval(function() {
-        $("#geolocate .ui-btn-text").html("Locating" + Geolocate.multiply(".", Geolocate.loadingStatus, "&#160;", 3));
-        Geolocate.loadingStatus += 1;
-        Geolocate.loadingStatus %= 4;
-      },500);
-    } else {
-      clearInterval(Geolocate.interval);
-    }
+  
+  stop: function(el) {
+    el = $(el).hide();
+    el.find(".ui-progressbar-value").stop();
+  }
+}
+
+
+
+var Geolocate = {
+  geolocateButton:null,
+  geolocateAbortButton:null,
+  geolocate:true,
+
+  init: function() {
+    this.geolocateButton = $("#geolocate");
+    this.geolocateAbortButton = $("#abort_geolocate");
+  
+    // Bind geolocate button click
+    this.geolocateButton.bind('click', function(event, ui) {
+      Geolocate.start();
+    });
+    
+    
+    // Hide abort button
+    this.geolocateAbortButton.hide().bind('click', function(event, ui) {
+      Geolocate.stop();
+    });
+    
+    
+    // Page show start geolocating
+    $("#foo").bind("pageshow", function() {
+      Geolocate.start();
+    });
   },
-  multiply: function(str, count, filler, filluptill) {
-    d = "";
-    for(i = 0; i < count; i++) d+=str;
-    for(; i < filluptill; i++) d+=filler;
-    return d;
+  
+  start: function() {
+    ProgressBar.init("#progressbar");
+    Geolocate.geolocateButton.hide();
+    Geolocate.geolocateAbortButton.show();
+    Geolocate.geolocate = setTimeout(Geolocate.success, 3000);
   },
+  
+  stop: function() {
+    ProgressBar.stop("#progressbar");
+    Geolocate.geolocateButton.show();
+    Geolocate.geolocateAbortButton.hide();
+    clearTimeout(Geolocate.geolocate);
+  },
+  
   success: function(position) {
-    Geolocate.setLoading(false);
-    $("#geolocate .ui-btn-text").html("Relocate");
-    $(".speech-a").empty().html('I found some parking lots near you! If you are not satisfied with the results try again by tapping the <b>Relocate</b> button or issue a <b>custom search</b> further down.');
-    Search.showResults('<li><a href="#three">Uni Passau Tiefgarage</a></li><li><a href="#three">Uni Passau Tiefgarage</a></li><li><a href="#three">Uni Passau Tiefgarage</a></li>');
+    Geolocate.stop();
+    $.mobile.changePage("#two");
+    $("#two ul").html(Utility.createList(["Parkplatz Universität", "Innstadt", "Einkaufszentrum"])).listview("refresh");
   },
   error: function(msg) {
     $("#geolocate .ui-btn-text").html("failed");
@@ -54,24 +89,24 @@ var Geolocate = {
 };
 
 var Search = {
-  data: ["Amberg","Ansbach","Aschaffenburg","Augsburg","Bamberg","Bayreuth","Coburg","Erlangen","Fürth","Hof","Ingolstadt","Kaufbeuren","Kempten (Allgäu)","Landshut","Memmingen","München Landeshauptstadt","Nürnberg","Passau","Regensburg","Rosenheim","Schwabach","Schweinfurt","Straubing","Weiden in der Oberpfalz","Würzburg"],
   init: function() {
     $("#search").bind("change", function(event, ui) {
       $.mobile.changePage($("#two"));
-      $("#two ul").empty();
-      var needle = "^"+$("#search").val().toLowerCase();
-      $.each(Search.data, function(i, el) {
-        if(el.toLowerCase().match(needle) != null) {
-          availability = Math.floor(Math.random()*100);
-          $("#two ul").append('<li><a href="#three"><h3>' + el + ' Tiefgarage</h3><p><strong></strong></p><span class="ui-li-count" style="color:#'+Color.transition(0x00FF00, 0xFF0000 ,availability/100).toString(16) +';">' + availability + '%</span></a></li>');
-        }
-      });
-      $("#two ul").listview('refresh');
+      $.mobile.changePage("#two");
+      $("#two ul").html(Utility.createList(["Parkplatz Universität", "Innstadt", "Einkaufszentrum"])).listview("refresh");
     });
   },
   
   showResults: function(data) {
     $("#fastresults").html(data).listview('refresh');
+  }
+};
+
+var Utility = {
+  createList: function(arr) {
+    str = "";
+    $.each(arr, function(i, el) { str+= "<li><a href=\"#three\">"+el+"</a></li>"; });
+    return str;
   }
 };
 
--- mockup/javascript/lot.js
+++ mockup/javascript/lot.js
@@ -1,51 +1,72 @@
 var Lot = {
-	parent: null,
-	data:null,
-	lot: [],
+  parent: null,
+  data:null,
+  lot: [],
 
-	/*
-	 * Create a parking view into dest with the given data. Data is a hash with properties:
-	 * width,height, lots (array of points)
-	 */
-	init: function(dest, data) {
-		this.parent = dest;
-		this.data = data;
-		this.buildTable();
-		this.colorLots();
-		Lot.resized();
-		$(window).resize(Lot.resized);
-	},
+  /*
+   * Create a parking view into dest with the given data. Data is a hash with properties:
+   * width,height, lots (array of points)
+   */
+  init: function(dest, data) {
+    this.parent = dest;
+    this.data = data;
+    this.buildTable();
+    this.colorLots();
+    Lot.resized();
+    $(window).resize(Lot.resized);
+    
+    $("#three").bind("pageshow", function() {
+      Lot.resized();
+    });
+  },
 
-	buildTable: function() {
-		$(this.parent).append("<table id=\"lot\"></table>");
-		table = $(this.parent).find("table");
-		for(y = 0; y < this.data.height; y++) {
-			row_collection = [];
-			row = document.createElement('tr');
-			for(x = 0; x < this.data.width; x++) {
-				cell = document.createElement('td');
-				row.appendChild(cell);
-				row_collection.push(cell);
-			}
-			table.append(row);
-			this.lot.push(row_collection);
-		}
+  buildTable: function() {
+    $(this.parent).append("<table id=\"lot\"></table>");
+    table = $(this.parent).find("table");
+    for(y = 0; y < this.data.height; y++) {
+      row_collection = [];
+      row = document.createElement('tr');
+      for(x = 0; x < this.data.width; x++) {
+        cell = document.createElement('td');
+        row.appendChild(cell);
+        row_collection.push(cell);
+      }
+      table.append(row);
+      this.lot.push(row_collection);
+    }
 
-		$(this.parent).append(table);
-	},
+    $(this.parent).append(table);
+  },
 
-	colorLots: function() {
-		for(i = 0; i < this.data.lots.length; i++) {
-			lot = this.data.lots[i];
-			$(this.lot[lot[1]][lot[0]]).css('background-color', 'black');
-		}
-	},
+  colorLots: function() {
+    for(i = 0; i < this.data.lots.length; i++) {
+      lot = this.data.lots[i];
+      
+      cl = "";
+      if(lot[2]) {
+        cl = "free";
+      } else { cl = "used"; }
+      $(this.lot[lot[1]][lot[0]]).addClass(cl);
+    }
+  },
 
-	resized: function() {
-		$('#lot td').css('height', $("#lot").width()/Lot.data.width);
-	}
+  resized: function() {
+    $('#lot td').css('height', $("#lot").width()/Lot.data.width);
+  },
+  
+  testLot: function(w, h) {
+    arr = [];
+    for(i = 0; i < w; i+=2) {
+      for(y = 0; y < h; y++) {
+        if(y % 5 != 0) {
+          arr.push([i, y, Math.random() < 0.5]);
+        }
+      }
+    }
+    return arr;
+  }
 };
 
 $(function() {
-	Lot.init("#lotdisplay", {width: 20, height: 30,lots: [[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[3,1],[3,2],[3,3],[3,4],[3,5],[3,6]]});
+  Lot.init("#lotdisplay", {width: 20, height: 30,lots: Lot.testLot(20,30)});
 });
