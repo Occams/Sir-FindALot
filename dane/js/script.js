0a0a1f73de62cee93dfe11ec7d537ec2c817c1f9
Dane <daniel.watzinger@gmail.com>
2011-11-18 15:41:42 +0100

    dane zeigs

---
+++ dane/js/script.js
@@ -0,0 +1,34 @@
+xui.ready(function() {
+	// Number of pages
+	var pageNum = 4;
+	x$('#slide_container').setStyle('width', pageNum+'00%');
+	
+	// Set .page width based on device
+	x$('.page').setStyle('width', window.innerWidth+'px');
+	
+	// Set active page
+	x$("#slide_container").setStyle('left','-'+window.innerWidth+'px');
+	
+	// Needs testing
+	x$('.page').on('orientationchange',function(e) {
+		x$('.page').setStyle('width', window.innerWidth+'px');
+	});
+	
+	// Register page animation events on buttons (TODO: change to touchstart)
+	x$("#btn_home").click(function(e) {
+		x$("#slide_container").setStyle('left','-'+window.innerWidth+'px');
+	})
+	
+	//MBP.fastButton(x$("#btn_home")[0], function() {
+	//		x$("#slide_container").setStyle('left','-'+window.innerWidth+'px');
+	//});
+	
+	x$("#btn_help").click(function(e) {
+		x$("#slide_container").setStyle('left','0px');
+	})
+	
+	x$("#btn_results").click(function(e) {
+		x$("#slide_container").setStyle('left','-'+2*window.innerWidth+'px');
+	})
+	
+});
