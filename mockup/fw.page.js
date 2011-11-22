4c370c5848c4080fe6268cf69c04e8255177883f
Bastian Huber <huber.baste@googlemail.com>
2011-11-22 14:48:03 +0100

    umstieg auf xui

--- mockup/fw.page.js
+++ mockup/fw.page.js
@@ -1,4 +1,5 @@
-(function($) {
+$ = x$;
+
 	// type is one of [slideinfromleft, slideouttoleft, slideinfromright, slideouttoright]
 	$.fn.css3Slide = function(slideType) {
 		this.each(function() {
@@ -13,10 +14,66 @@
 		});
 	}
 	
+	$.fn.show = function() {
+		this.setStyle("display","block");
+		return this;
+	}
+	
+	$.fn.hide = function() {
+		this.setStyle("display","none");
+		return this;
+	}
+	
+	$.fn.dimensions = function() {
+    element = this[0];
+    var display = this.getStyle('display');
+    
+    if (display && display !== 'none') {
+      return { width: element.offsetWidth, height: element.offsetHeight };
+    }
+    
+    // All *Width and *Height properties give 0 on elements with
+    // `display: none`, so show the element temporarily.
+    /*
+    var style = element.style;
+    var originalStyles = {
+      visibility: style.visibility,
+      position:   style.position,
+      display:    style.display
+    };
+    
+    var newStyles = {
+      visibility: 'hidden',
+      display:    'block'
+    };
+
+    // Switching `fixed` to `absolute` causes issues in Safari.
+    if (originalStyles.position !== 'fixed')
+      newStyles.position = 'absolute';
+    
+    Element.setStyle(element, newStyles);
+    */
+    var dimensions = {
+      width:  element.offsetWidth,
+      height: element.offsetHeight
+    };
+    /*
+    Element.setStyle(element, originalStyles);
+    */
+
+    return dimensions;
+	}
+	
+	$.fn.width = function() {
+		return this.dimensions().width;
+	}
+	
+	$.fn.height = function() {
+		return this.dimensions().height;
+	}
+	
 	$.fn.css3Slide.types = "slide in out reverse";
 	$.fn.css3Slide.typesToClass = {"slideinfromleft":"slide in reverse", "slideouttoleft":"slide out", "slideinfromright":"slide in", "slideouttoright":"slide out reverse"};
-})(jQuery);
-
 
 String.prototype.idify = function() { return (this[0]=="#")?this.slice(1):this; }
 String.prototype.hashify = function() { return (this[0]=="#")?this:"#"+this; };
@@ -37,7 +94,7 @@ var Page = {
 	init: function() {
 		this.pages = $("#pages div[data-type=page]");
 		this.footer = $("#pages footer");
-		this.pages.each(function(i, el) {
+		this.pages.each(function(el, i) {
 			Page.id_page_hash[el.getAttribute("id")] = el;
 			Page.id_pagenum_hash[el.getAttribute("id")] = i;
 		});
@@ -45,18 +102,20 @@ var Page = {
 		this.gFooter = $("#global-footer");
 		
 		hash = window.location.hash;
-		this.show(this._isPage(hash)?hash:this.pages.first().attr("id")); // Show the first page initialliy
+		this.show(this._isPage(hash)?hash:this.pages[0].getAttribute("id")); // Show the first page initialliy
 		
-		this.window_width = $(window).width();
+		this.window_width = window.innerWidth;
 		this.layout();
 	},
 	
 	layout: function() {
-		this.footer.css("width", this.window_width);
+		this.footer.setStyle("width", this.window_width);
 	},
 	
 	show: function(id) {
-		if(!this._lock() || !this._isPage(id)) return;
+		if(id=="") id = this.pages[0].id; // If hashchange to no hash show first page
+		if(!this._isPage(id)) return;
+		if(!this._lock()) return;
 	
 		if(this.current_page == null) { // If you set the page for the first time...
 			this.current_page = id;
@@ -82,12 +141,12 @@ var Page = {
 	
 	_updatePageInfos: function(id) {
 		page = this._getPage(id);
-		this.gHeader.html(page.find("header").html());
-		this.gFooter.html(page.find("footer").html());
+		this.gHeader.html(page.find("header")[0].innerHTML);
+		this.gFooter.html(page.find("footer")[0].innerHTML);
 		
 		//Toolbar width
 		els = this.gFooter.find(".toolbar li");
-		this.gFooter.find(".toolbar").css("width", els.length*(els.first().width()+3));
+		this.gFooter.find(".toolbar").setStyle("width", els.length*($(els[0]).width()+3));
 		
 		// Fastbutton update for toolbar links
 		this.gFooter.find("a").fastbutton(function(event) {
@@ -96,7 +155,8 @@ var Page = {
 			return false;
 		});
 		
-		$("a[href*=#]").click(this._stopHash);
+		
+		$("a[href*='#']").click(Page._stopHash);
 	},
 	
 	_getPage: function(id) {
@@ -113,10 +173,10 @@ var Page = {
 	
 	_finishedAnimation: function(event) {
 		// Hide all unused pages so that scrolling works just right...
-		Page.pages.each(function(i, el) {
+		Page.pages.each(function(el, i) {
 			if(el.getAttribute("id") != Page.current_page) $(el).hide();
 		});
-		Page._getPage(Page.current_page).trigger("pageshow");
+		Page._getPage(Page.current_page).fire("pageshow");
 		window.location.hash = Page.current_page.hashify();
 		Page._unlock();
 	},
@@ -139,14 +199,14 @@ var Page = {
 	}
 };
 
-//hash dings
-$(window).hashchange(function(event) {
+window.onhashchange = function(event) {
 	event.preventDefault();
+	console.log(window.location.hash.idify());
 	Page.show(window.location.hash.idify());
 	return false;
-});
+};
 
-$(function() {
+$.ready(function() {
 	Page.init();
 	
 	$(".link-list a").fastbutton(function(event) {
