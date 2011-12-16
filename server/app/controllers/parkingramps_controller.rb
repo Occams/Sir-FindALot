8033764c74a221e874814ae4929ca731bc12a482
Bastian Huber <huber.baste@googlemail.com>
2011-12-16 16:24:34 +0100

    Cross Site Scripting for public api

--- server/app/controllers/parkingramps_controller.rb
+++ server/app/controllers/parkingramps_controller.rb
@@ -1,5 +1,16 @@
+ACCESS_CONTROLS = {
+    "Access-Control-Allow-Origin" => "*",
+    "Access-Control-Allow-Headers" => "X-Requested-With",
+    "Access-Control-Max-Age" => "60",
+    "Access-Control-Request-Method" => '*' 
+}
+
 class ParkingrampsController < ApplicationController
   def show
     @parkingramp = Parkingramp.find(params[:id])
+    
+    ACCESS_CONTROLS.each do |k,v|
+      headers[k] = v
+    end
   end
 end
