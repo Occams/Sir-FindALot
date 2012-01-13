8cda496c5df15e411c60a9639ec23de33f3e856c
Bastian Huber <huber.baste@googlemail.com>
2012-01-13 13:19:36 +0100

    upgrade.html

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -6,14 +6,23 @@ class ApplicationController < ActionController::Base
     raise ActionController::RoutingError.new('Not Found')
   end
   
+protected
+  def ismobile
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+    MOBILE_BROWSERS.each do |m|
+      return true if agent.match(m)
+    end
+    return true # set to false if you want to see website TODO!
+  end
+  
 private
   MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
 
   def detect_browser
-    agent = request.headers["HTTP_USER_AGENT"].downcase
-    MOBILE_BROWSERS.each do |m|
-      return "mobile" if agent.match(m)
+    if ismobile
+      return "mobile"
+    else
+      return "application"
     end
-    return "mobile"
   end
 end
